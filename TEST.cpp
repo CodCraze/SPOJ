a15da29e77641e9dc660a391c334207d7dc73273
Code Craze <87587476+CodCraze@users.noreply.github.com>
2021-08-02 02:32:10 +0530

    Create TEST.cpp

---
+++ TEST.cpp
@@ -0,0 +1,24 @@
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("avx,avx2,fma")
+
+#include<bits/stdc++.h>
+
+#define endl "\n"
+#define inputv(x,n) for(ll i=0;i<n;i++) cin>>x[i];
+using namespace std;
+typedef long long ll;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	
+	ll d;
+	while(1){
+		cin>>d;
+		if(d==42)	break;
+		cout<<d<<endl;
+	}
+	
+	return 0;
+}
